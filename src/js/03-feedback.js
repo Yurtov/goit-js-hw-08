0dcee9b9ff2af763e7f259e6c59b43d9e2f3c7cc
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-06-17 19:45:50 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+const OBJECT_OF_INPUTS = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(OBJECT_OF_INPUTS)) || {};
 
+const formEl = document.querySelector('.feedback-form');
+populateInputeArea();
+console.log(formEl.elements);
+
+formEl.addEventListener('input', throttle(getInputChange, 500));
+formEl.addEventListener('submit', clearInputChange);
+
+function getInputChange(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(OBJECT_OF_INPUTS, JSON.stringify(formData));
+}
+
+function clearInputChange(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(OBJECT_OF_INPUTS);
+}
+
+function populateInputeArea() {
+  if (formData) {
+    formEl.elements.email.value = formData.email || '';
+    formEl.elements.message.value = formData.message || '';
+  }
+}
