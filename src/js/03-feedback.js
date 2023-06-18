c2350c8538b00971f59d4ad57e3e3a700ac8585f
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-06-18 12:21:07 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,9 @@
 import throttle from 'lodash.throttle';
 const OBJECT_OF_INPUTS = 'feedback-form-state';
-const formData = JSON.parse(localStorage.getItem(OBJECT_OF_INPUTS)) || {};
+let formData = JSON.parse(localStorage.getItem(OBJECT_OF_INPUTS)) || {};
 
 const formEl = document.querySelector('.feedback-form');
 populateInputeArea();
-console.log(formEl.elements);
 
 formEl.addEventListener('input', throttle(getInputChange, 500));
 formEl.addEventListener('submit', clearInputChange);
@@ -12,12 +11,27 @@ formEl.addEventListener('submit', clearInputChange);
 function getInputChange(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(OBJECT_OF_INPUTS, JSON.stringify(formData));
+  console.log('Local storege:', formData);
 }
 
 function clearInputChange(e) {
   e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem(OBJECT_OF_INPUTS);
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value.trim() === '' || message.value === '') {
+    alert('Please, fill in all the fields');
+  } else {
+    const inputDataObject = {
+      email: email.value,
+      message: message.value,
+    };
+    console.log('Data object:', inputDataObject);
+    e.currentTarget.reset();
+    localStorage.removeItem(OBJECT_OF_INPUTS);
+    formData = {};
+  }
 }
 
 function populateInputeArea() {
